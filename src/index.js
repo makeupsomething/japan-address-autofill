50ab138b70b9b545e8447e1d4b490f90f9826c2d
daryl <darylcoleoffice@gmail.com>
2018-09-30 23:32:58 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch');  
+
+const prefectureList = [
+    null,       '北海道',   '青森県',   '岩手県',   '宮城県',
+    '秋田県',   '山形県',   '福島県',   '茨城県',   '栃木県',
+    '群馬県',   '埼玉県',   '千葉県',   '東京都',   '神奈川県',
+    '新潟県',   '富山県',   '石川県',   '福井県',   '山梨県',
+    '長野県',   '岐阜県',   '静岡県',   '愛知県',   '三重県',
+    '滋賀県',   '京都府',   '大阪府',   '兵庫県',   '奈良県',
+    '和歌山県', '鳥取県',   '島根県',   '岡山県',   '広島県',
+    '山口県',   '徳島県',   '香川県',   '愛媛県',   '高知県',
+    '福岡県',   '佐賀県',   '長崎県',   '熊本県',   '大分県',
+    '宮崎県',   '鹿児島県', '沖縄県'
+];
+
+export function getAddressByZip(code) {
+    let prefCode = code.substring(0, 3)
+    let url = `https://yubinbango.github.io/yubinbango-data/data/${prefCode}.js`
+    return fetch(url)
+    .then((response) => {
+        return response.text();
+    })
+    .then((text) => {
+        let codes = JSON.parse(text.replace("$yubin(", "").replace(");", ""));
+        const { [code]:address } = codes
+        const [ prefId, city, area, street ] = address
+        return {
+            'prefecture': prefectureList[prefId],
+            city,
+            area,
+            street,
+        }
+    });
+}
