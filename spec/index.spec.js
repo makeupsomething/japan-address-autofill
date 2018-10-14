e1c6050eae4938fb6c45e4aaac34cf5db0f8a11f
daryl <darylcoleoffice@gmail.com>
2018-10-14 12:35:23 +0900

    Add tests

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -11,4 +11,26 @@ describe("Getting an address", function() {
             done();
         });
     });
+
+    it("getting an address with a valid zipcode that includes a hyphen", function(done) {
+        var japanAddressAutofill = require('../lib/index.js');
+        japanAddressAutofill.getAddressByZip("100-0005")
+        .then((response) => {
+            const { prefecture, city, area, street } = response
+            expect(prefecture).toBe("東京都");
+            expect(city).toBe("千代田区");
+            expect(area).toBe("丸の内");
+            expect(street).toBe(undefined);
+            done();
+        });
+    });
+
+    it("getting an address with a invalid zipcode", function(done) {
+        var japanAddressAutofill = require('../lib/index.js');
+        japanAddressAutofill.getAddressByZip("100000005")
+        .catch((error) => {
+            expect(error).toBeTruthy();
+            done();
+        })
+    });
 });
